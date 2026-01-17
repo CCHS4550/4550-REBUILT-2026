a76cc5685fbba07ac98e69ac588057f0d0e6a1b5
holyb3anS <144182597+holyb3anS@users.noreply.github.com>
2026-01-17 15:11:23 -0700

    HoodIOCTRE working on it

--- src/main/java/frc/robot/Subsystems/Turret/Hood/HoodIO.java
+++ src/main/java/frc/robot/Subsystems/Turret/Hood/HoodIO.java
@@ -1,6 +1,10 @@
-package frc.robot.subsystems.turret.hood;
+package frc.robot.Subsystems.Turret.Hood;
 import org.littletonrobotics.junction.AutoLog;
+
+import com.ctre.phoenix6.StatusSignal;
+
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.measure.AngularVelocity;
 
 
 public interface HoodIO {
@@ -8,11 +12,13 @@ public interface HoodIO {
     @AutoLog
     public class HoodIOInputs {
         public double hoodVoltage = 0.0;
-        public double hoodSupplyCurrent = 0.0;
+        public double hoodSupplyCurrent = 0.0; 
         public double hoodStatorCurrent = 0.0;
         public double hoodTemperature = 0.0;
+
         public double hoodVelocityRotPerSec = 0.0;
         public double hoodAccelRotPerSecSquared = 0.0;
+
         public double hoodVelocityRadPerSec = 0.0;
         public double hoodAccelRadPerSecSquared = 0.0;
 
