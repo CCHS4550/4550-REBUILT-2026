0afc16d2dcb4e93a4617409e6835f9f3b27174df
holyb3anS <144182793+RuhanRajasthan@users.noreply.github.com>
2026-01-22 17:46:30 -0700

    12-agitator

--- src/main/java/frc/robot/Subsystems/Agitator/Agitator.java
+++ src/main/java/frc/robot/Subsystems/Agitator/Agitator.java
@@ -1,17 +1,54 @@
 package frc.robot.Subsystems.Agitator;
-
+public class Agitator extends SubsystemBase implements AgitatorIO {
 public class Agitator {
-    public enum WantedState {
-        IDLE,
-        SPINNING
+  public enum WantedState {
+    IDLE,
+    SPINNING
+  }
+
+  public enum SystemState {
+    IDLE,
+    SPINNING
+  }
+
+  private SystemState systemState = SystemState.IDLE;
+  private WantedState wantedState = WantedState.IDLE;
+
+  private final AgitatorIO agitatorIO;
+  @Override
+  public void periodic(){
+        systemState = handleSystemState();
+        applyWantedState();
+  }
+
+  private SystemState handleSystemState() {
+    switch (systemState) {
+      case IDLE:
+        if (wantedState == WantedState.SPINNING) {
+          return SystemState.SPINNING;
+        }
+        break;
+      case SPINNING:
+        if (wantedState == WantedState.IDLE) {
+          return SystemState.IDLE;
+        }
+        break;
+    }
+    return systemState;
+  }
+
+    private void applyWantedState() {
+        switch (systemState) {
+        case IDLE:
+            agitatorIO.setVoltage(0.0);
+            break;
+        case SPINNING:
+            agitatorIO.setVoltage(12.0);
+            break;
+        }
+
     }
 
-    public enum SystemState {
-        IDLE,
-        SPINNING
-    }
-
-    private SystemState systemState = SystemState.IDLE;
-    private WantedState wantedState = WantedState.IDLE;
     
+}
 }
--- src/main/java/frc/robot/Subsystems/Agitator/AgitatorIO.java
+++ src/main/java/frc/robot/Subsystems/Agitator/AgitatorIO.java
@@ -3,22 +3,23 @@ package frc.robot.Subsystems.Agitator;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface AgitatorIO {
-    @AutoLog
-    public class AgitatorIOInputs {
-        public double agitatorVoltage = 0.0;
-        public double agitatorCurrent = 0.0;
-        public double agitatorSpeedRPS = 0.0;
+  @AutoLog
+  public class AgitatorIOInputs {
+    public double agitatorVoltage = 0.0;
+    public double agitatorCurrent = 0.0;
+    public double agitatorSpeedRPS = 0.0;
 
-        public double temperature = 0.0;
-        public double rotationVelocityRotPerSec = 0.0;
-        public double rotationAccelRotPerSecSquared = 0.0;
-        public double rotationVelocityRadPerSec = 0.0;
-        public double rotationAccelRadPerSecSquared = 0.0;
+    public double temperature = 0.0;
+    public double rotationVelocityRotPerSec = 0.0;
+    public double rotationAccelRotPerSecSquared = 0.0;
+    public double rotationAccelRadPerSecSquared = 0.0;
+  }
 
-    }
+  public default void updateInputs(AgitatorIOInputs inputs) {}
 
-    default void updateInputs(AgitatorIOInputs inputs) {}
-    
-    default void setVoltage(double voltage) {}
+  public default void setVoltage(double voltage) {}
 
+  public default void stop(){
+    setVoltage(0.0);
+  }
 }
