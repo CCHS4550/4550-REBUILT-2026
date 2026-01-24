c55d778a251b5a9cfbfd37847658c59844519245
holyb3anS <144182597+holyb3anS@users.noreply.github.com>
2026-01-24 13:46:01 -0700

    update information updates and autologged logic

--- src/main/java/frc/robot/Subsystems/Agitator/Agitator.java
+++ src/main/java/frc/robot/Subsystems/Agitator/Agitator.java
@@ -1,6 +1,8 @@
 package frc.robot.Subsystems.Agitator;
-public class Agitator extends SubsystemBase implements AgitatorIO {
-public class Agitator {
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Agitator extends SubsystemBase {
   public enum WantedState {
     IDLE,
     SPINNING
@@ -15,40 +17,36 @@ public class Agitator {
   private WantedState wantedState = WantedState.IDLE;
 
   private final AgitatorIO agitatorIO;
+
+  private AgitatorIOInputsAutoLogged inputs = new AgitatorIOInputsAutoLogged();
+
+  public Agitator(AgitatorIO agitatorIO) {
+    this.agitatorIO = agitatorIO;
+  }
+
   @Override
-  public void periodic(){
-        systemState = handleSystemState();
-        applyWantedState();
+  public void periodic() {
+    agitatorIO.updateInputs(inputs);
+
+    systemState = handleSystemState();
+    applyWantedState();
   }
 
   private SystemState handleSystemState() {
-    switch (systemState) {
-      case IDLE:
-        if (wantedState == WantedState.SPINNING) {
-          return SystemState.SPINNING;
-        }
-        break;
-      case SPINNING:
-        if (wantedState == WantedState.IDLE) {
-          return SystemState.IDLE;
-        }
-        break;
-    }
-    return systemState;
+    return switch (wantedState) {
+      case IDLE -> SystemState.IDLE;
+      case SPINNING -> SystemState.SPINNING;
+    };
   }
 
-    private void applyWantedState() {
-        switch (systemState) {
-        case IDLE:
-            agitatorIO.setVoltage(0.0);
-            break;
-        case SPINNING:
-            agitatorIO.setVoltage(12.0);
-            break;
-        }
-
+  private void applyWantedState() {
+    switch (systemState) {
+      case IDLE:
+        agitatorIO.setVoltage(0.0);
+        break;
+      case SPINNING:
+        agitatorIO.setVoltage(5.0);
+        break;
     }
-
-    
-}
+  }
 }
--- src/main/java/frc/robot/Subsystems/Agitator/AgitatorIOCTRE.java
+++ src/main/java/frc/robot/Subsystems/Agitator/AgitatorIOCTRE.java
@@ -1,31 +1,32 @@
 package frc.robot.Subsystems.Agitator;
 
+import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularAcceleration;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Temperature;
 import edu.wpi.first.units.measure.Voltage;
+import frc.robot.Config.BruinRobotConfig;
 
 public class AgitatorIOCTRE implements AgitatorIO {
   private TalonFX agitatorMotor;
   private TalonFXConfiguration agitatorConfig;
-  private MotionMagicVoltage motionMagicVoltage = new MotionMagicVoltage(0).withSlot(0);
   private final StatusSignal<Voltage> agitatorAppliedVolts;
   private final StatusSignal<Current> agitatorSupplyCurrentAmps;
   private final StatusSignal<Current> agitatorStatorCurrentAmps;
-  private final StatusSignal<AngularVelocity> agitatorVelocityPerSec;
-  private final StatusSignal<AngularAcceleration> agitatorAccelerationPerSecSquared;
+  private final StatusSignal<Angle> agitatorPositionRotations;
+  private final StatusSignal<AngularVelocity> agitatorVelocityRotationsPerSec;
+  private final StatusSignal<AngularAcceleration> agitatorAccelerationRotationsPerSecSquared;
   private final StatusSignal<Temperature> agitatorMotorTemp;
 
-  public AgitatorIOCTRE(int agitatorID, String canBusString) {
-    agitatorMotor = new TalonFX(agitatorID, canBusString);
+  public AgitatorIOCTRE(BruinRobotConfig bruinRobotConfig) {
+    agitatorMotor = new TalonFX(0);
     agitatorConfig = new TalonFXConfiguration();
 
     agitatorConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
@@ -33,27 +34,52 @@ public class AgitatorIOCTRE implements AgitatorIO {
     agitatorConfig.CurrentLimits.SupplyCurrentLimit = 0.0;
     agitatorConfig.CurrentLimits.StatorCurrentLimit = 0.0;
 
-    agitatorConfig.Slot0.kP = 0.0;
-    agitatorConfig.Slot0.kI = 0.0;
-    agitatorConfig.Slot0.kD = 0.0;
-
     agitatorConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-    agitatorConfig.MotionMagic.MotionMagicAcceleration = 0.0;
-    agitatorConfig.MotionMagic.MotionMagicCruiseVelocity = 0.0;
     agitatorConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
 
     agitatorMotor.getConfigurator().apply(agitatorConfig);
+
+    agitatorMotor.setPosition(0.0);
+    agitatorPositionRotations = agitatorMotor.getPosition();
+    agitatorAppliedVolts = agitatorMotor.getMotorVoltage();
+    agitatorSupplyCurrentAmps = agitatorMotor.getSupplyCurrent();
+    agitatorStatorCurrentAmps = agitatorMotor.getStatorCurrent();
+    agitatorVelocityRotationsPerSec = agitatorMotor.getVelocity();
+    agitatorAccelerationRotationsPerSecSquared = agitatorMotor.getAcceleration();
+    agitatorMotorTemp = agitatorMotor.getDeviceTemp();
   }
 
-    @Override
-    public void updateInputs(AgitatorIOInputs inputs) {
-        // TODO Auto-generated method stub
+  @Override
+  public void updateInputs(AgitatorIOInputs inputs) {
+    BaseStatusSignal.refreshAll(
+        agitatorPositionRotations,
+        agitatorAppliedVolts,
+        agitatorSupplyCurrentAmps,
+        agitatorStatorCurrentAmps,
+        agitatorVelocityRotationsPerSec,
+        agitatorAccelerationRotationsPerSecSquared,
+        agitatorMotorTemp);
 
-    }
+    inputs.agitatorPositionRadians =
+        agitatorPositionRotations.getValueAsDouble()
+            * 2
+            * Math.PI; // TODO: Update this later with a total gear reduction in constants
+    inputs.agitatorVoltage = agitatorAppliedVolts.getValueAsDouble();
+    inputs.agitatorSupplyCurrent = agitatorSupplyCurrentAmps.getValueAsDouble();
+    inputs.agitatorStatorCurrent = agitatorStatorCurrentAmps.getValueAsDouble();
+    inputs.rotationVelocityRadPerSec =
+        agitatorVelocityRotationsPerSec.getValueAsDouble()
+            * 2
+            * Math.PI; // TODO: Update this later with a total gear reduction in constants
+    inputs.rotationAccelRadPerSecSquared =
+        agitatorAccelerationRotationsPerSecSquared.getValueAsDouble()
+            * 2
+            * Math.PI; // TODO: Update this later with a total gear reduction in constants
+    inputs.temperature = agitatorMotorTemp.getValueAsDouble();
+  }
 
-    @Override
-    public void setVoltage(double voltage) {
-        // TODO Auto-generated method stub
-        agitatorMotor.setControl(PositionVoltage.ofVoltage(voltage));
-    }
+  @Override
+  public void setVoltage(double voltage) {
+    agitatorMotor.setVoltage(voltage);
+  }
 }
