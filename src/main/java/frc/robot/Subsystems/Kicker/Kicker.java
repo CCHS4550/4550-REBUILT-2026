7662761856c9cc800c053f4193d71bba12ec0620
TerryZhang12 <greninja7144@gmail.com>
2026-01-24 09:56:15 -0700

    idk what i changed

--- src/main/java/frc/robot/Subsystems/Kicker/Kicker.java
+++ src/main/java/frc/robot/Subsystems/Kicker/Kicker.java
@@ -2,64 +2,58 @@ package frc.robot.Subsystems.Kicker;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Kicker extends SubsystemBase
-{
-    private KickerIO kickerIO;
+public class Kicker extends SubsystemBase {
+  private KickerIO kickerIO;
 
-    private boolean atGoal;
+  private boolean atGoal;
 
-    private KickerIOInputsAutoLogged kickerInputs = new KickerIOInputsAutoLogged();
+  private KickerIOInputsAutoLogged kickerInputs = new KickerIOInputsAutoLogged();
 
-    public enum KickerSystemState
-    {
-        IDLE,
-        KICK
+  public enum KickerSystemState {
+    IDLE,
+    KICK
+  }
+
+  public enum KickerWantedState {
+    IDLE,
+    KICK
+  }
+
+  private KickerSystemState systemState = KickerSystemState.IDLE;
+  private KickerWantedState wantedState = KickerWantedState.IDLE;
+
+  public Kicker(KickerIO io) {
+    this.kickerIO = io;
+
+    atGoal = true;
+  }
+
+  @Override
+  public void periodic() {
+    kickerIO.updateInputs(kickerInputs);
+  }
+
+  public KickerSystemState handleStateTransitions() {
+    switch (wantedState) {
+      case IDLE:
+        return KickerSystemState.IDLE;
+      case KICK:
+        return KickerSystemState.KICK;
+      default:
+        return KickerSystemState.IDLE;
     }
+  }
 
-    public enum KickerWantedState
-    {
-        IDLE,
-        KICK
-    }
-
-    private KickerSystemState systemState = KickerSystemState.IDLE;
-    private KickerWantedState wantedState = KickerWantedState.IDLE;
-
-    public Kicker(KickerIO io)
-    {
-        this.kickerIO = io;
-
+  public void applyStates() {
+    switch (systemState) {
+      case IDLE:
         atGoal = true;
+        break;
+      case KICK:
+        // kicking logic
+        break;
+      default:
+        break;
     }
-
-    @Override
-    public void periodic()
-    {
-        kickerIO.updateInputs(kickerInputs);
-
-    }
-
-    public KickerSystemState handleStateTransitions()
-    {
-        switch(wantedState)
-        {
-            case IDLE: return KickerSystemState.IDLE;
-            case KICK: return KickerSystemState.KICK;
-            default: return KickerSystemState.IDLE;
-        }
-    }
-
-    public void applyStates()
-    {
-        switch(systemState)
-        {
-            case IDLE:
-                atGoal = true;
-                break;
-            case KICK: 
-                //kicking logic
-                break;
-            default: break;
-        }
-    }
+  }
 }
--- src/main/java/frc/robot/Subsystems/Kicker/KickerIOCTRE.java
+++ src/main/java/frc/robot/Subsystems/Kicker/KickerIOCTRE.java
@@ -6,7 +6,6 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
-
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularAcceleration;
 import edu.wpi.first.units.measure.AngularVelocity;
@@ -16,61 +15,54 @@ import edu.wpi.first.units.measure.Voltage;
 import frc.robot.Config.BruinRobotConfig;
 import frc.robot.Util.Phoenix6Util;
 
-public class KickerIOCTRE implements KickerIO
-{
-    private TalonFX kickerMotor;
-    private CANcoder kickerEncoder;
-    private TalonFXConfiguration kickerConfig;
-    private CANcoderConfiguration encoderConfig;
-    private MotionMagicVoltage motionMagicVoltage = new MotionMagicVoltage(0).withSlot(0);
-    private final StatusSignal<Angle> kickerAngleRotations;
-    private final StatusSignal<Voltage> kickerAppliedVolts;
-    private final StatusSignal<Current> kickerSupplyCurrentAmps;
-    private final StatusSignal<Current> kickerStatorCurrentAmps;
-    private final StatusSignal<AngularVelocity> kickerVelocityRotationsPerSec;
-    private final StatusSignal<AngularAcceleration> kickerAccelerationRotationsPerSecSquared;
-    private final StatusSignal<Temperature> kickerMotorTemp;
+public class KickerIOCTRE implements KickerIO {
+  private TalonFX kickerMotor;
+  private CANcoder kickerEncoder;
+  private TalonFXConfiguration kickerConfig;
+  private CANcoderConfiguration encoderConfig;
+  private MotionMagicVoltage motionMagicVoltage = new MotionMagicVoltage(0).withSlot(0);
+  private final StatusSignal<Angle> kickerAngleRotations;
+  private final StatusSignal<Voltage> kickerAppliedVolts;
+  private final StatusSignal<Current> kickerSupplyCurrentAmps;
+  private final StatusSignal<Current> kickerStatorCurrentAmps;
+  private final StatusSignal<AngularVelocity> kickerVelocityRotationsPerSec;
+  private final StatusSignal<AngularAcceleration> kickerAccelerationRotationsPerSecSquared;
+  private final StatusSignal<Temperature> kickerMotorTemp;
 
-    public KickerIOCTRE(BruinRobotConfig robotConfig)
-    {
+  public KickerIOCTRE(BruinRobotConfig robotConfig) {
 
-        //Hopefully these kicker attributes gets created later in BruinRobotConfig.java
-        kickerMotor = new TalonFX(
-            robotConfig.KICKER_MOTOR.getDeviceNumber(),
-            robotConfig.KICKER_MOTOR.getBus());
+    // Hopefully these kicker attributes gets created later in BruinRobotConfig.java
+    // kickerMotor = new TalonFX(robotConfig.KICKER_MOTOR.getDeviceNumber(),
+    // robotConfig.KICKER_MOTOR.getBus());
+    kickerMotor = new TalonFX(-1, "");
+    // kickerEncoder = new CANcoder(robotConfig.KICKER_CANCODER.getDeviceNumber(),
+    // robotConfig.KICKER_CANCODER.getBus());
+    kickerEncoder = new CANcoder(-1, "");
 
-        kickerEncoder = new CANcoder(
-            robotConfig.KICKER_CANCODER.getDeviceNumber(),
-            robotConfig.KICKER_CANCODER.getBus());
+    kickerConfig = new TalonFXConfiguration();
+    // do config stuff
+    Phoenix6Util.applyAndCheckConfiguration(kickerMotor, kickerConfig);
 
-        kickerConfig = new TalonFXConfiguration();
-        //do config stuff
-        Phoenix6Util.applyAndCheckConfiguration(kickerMotor, kickerConfig);
+    encoderConfig = new CANcoderConfiguration();
+    // do more config stuff
+    kickerEncoder.getConfigurator().apply(encoderConfig);
 
-        encoderConfig = new CANcoderConfiguration();
-        //do more config stuff
-        kickerEncoder.getConfigurator().apply(encoderConfig);
+    kickerAngleRotations = kickerEncoder.getAbsolutePosition();
+    kickerAppliedVolts = kickerMotor.getMotorVoltage();
+    kickerSupplyCurrentAmps = kickerMotor.getSupplyCurrent();
+    kickerStatorCurrentAmps = kickerMotor.getStatorCurrent();
+    kickerVelocityRotationsPerSec = kickerEncoder.getVelocity();
+    kickerAccelerationRotationsPerSecSquared = kickerMotor.getAcceleration();
+    kickerMotorTemp = kickerMotor.getDeviceTemp();
+  }
 
-        kickerAngleRotations = kickerEncoder.getAbsolutePosition();
-        kickerAppliedVolts = kickerMotor.getMotorVoltage();
-        kickerSupplyCurrentAmps = kickerMotor.getSupplyCurrent();
-        kickerStatorCurrentAmps = kickerMotor.getStatorCurrent();
-        kickerVelocityRotationsPerSec = kickerEncoder.getVelocity();
-        kickerAccelerationRotationsPerSecSquared = kickerMotor.getAcceleration();
-        kickerMotorTemp = kickerMotor.getDeviceTemp();
-    }
+  @Override
+  public void updateInputs(KickerIOInputs inputs) {
+    // too lazy to do this rn
+  }
 
-    @Override
-    public void updateInputs(KickerIOInputs inputs)
-    {
-        //too lazy to do this rn
-    }
-
-
-    
-    @Override
-    public void setVoltage(double volts)
-    {
-        kickerMotor.setVoltage(volts);
-    }
+  @Override
+  public void setVoltage(double volts) {
+    kickerMotor.setVoltage(volts);
+  }
 }
