559b68971a2f84b18104d4e6b786cad152ff15c1
adah <adahmeyerhoff@gmail.com>
2026-01-26 17:01:34 -0700

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,12 +1,18 @@
 package frc.robot;
 
+import java.lang.ModuleLayer.Controller;
+
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Config.BruinRobotConfig;
 import frc.robot.Subsystems.Drive.SwerveIOCTRE;
 import frc.robot.Subsystems.Drive.SwerveSubsystem;
+import frc.robot.Subsystems.Drive.SwerveSubsystem.WantedState;
 
 public class RobotContainer {
   private final SwerveSubsystem swerveSubsystem;
@@ -33,6 +39,10 @@ public class RobotContainer {
             controller,
             1.5,
             1.5 / Math.hypot(moduleConstants[0].LocationX, moduleConstants[0].LocationY));
+
+    controller.a().onTrue(new InstantCommand(() -> swerveSubsystem.setTargetRotation(Rotation2d.fromDegrees(0))));
+    controller.a().onTrue(new InstantCommand(() -> swerveSubsystem.setWantedState(WantedState.ROTATION_LOCK)));
+    controller.a().onFalse(new InstantCommand(() -> swerveSubsystem.setWantedState(WantedState.TELEOP_DRIVE)));
   }
 
   public SwerveSubsystem getSwerveSubsystem() {
