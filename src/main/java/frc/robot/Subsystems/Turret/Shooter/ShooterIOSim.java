bec9196b75f1fbf82260abce3114fdeaa0c7e32d
RuhanRajasthan <144182793+RuhanRajasthan@users.noreply.github.com>
2026-02-17 20:51:18 -0700

    working on simulation
    my computer doesn't build code so there are probably like a million errors

---
+++ src/main/java/frc/robot/Subsystems/Turret/Shooter/ShooterIOSim.java
@@ -0,0 +1,118 @@
+package frc.robot.Subsystems.Turret.Shooter;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+import frc.robot.Config.BruinRobotConfig;
+import frc.robot.Util.Phoenix6Util;
+
+public class ShooterIOSim implements ShooterIO {
+
+  private TalonFX shooterMotor;
+  private TalonFXSimState shooterMotorSim;
+  private TalonFX shooterMotor2;
+  private TalonFXSimState shooterMotor2Sim;
+
+  private FlywheelSim shooterSimFlywheel;
+
+  private TalonFXConfiguration shooterConfig;
+  private MotionMagicVelocityVoltage motionMagicVelocityVoltage;
+
+  private final StatusSignal<Voltage> shooterAppliedVoltage;
+  private final StatusSignal<Current> shooterSupplyCurrent;
+  private final StatusSignal<Current> shooterStatorCurrent;
+  private final StatusSignal<AngularVelocity> shooterVelocityRotationsPerSec;
+  private final StatusSignal<AngularAcceleration> shooterAccelerationRotationsPerSecSquared;
+
+
+  public ShooterIOSim(BruinRobotConfig bruinRobotConfig) {
+    Follower follower =
+        new Follower(bruinRobotConfig.SHOOTER_MOTOR.getDeviceNumber(), MotorAlignmentValue.Aligned);
+    shooterMotor =
+        new TalonFX(
+            bruinRobotConfig.SHOOTER_MOTOR.getDeviceNumber(),
+            bruinRobotConfig.SHOOTER_MOTOR.getBus());
+    shooterMotor2 =
+        new TalonFX(
+            bruinRobotConfig.SHOOTER_MOTOR_2.getDeviceNumber(),
+            bruinRobotConfig.SHOOTER_MOTOR_2.getBus());
+    shooterMotorSim = shooterMotor.getSimState();
+    motionMagicVelocityVoltage = new MotionMagicVelocityVoltage(0.0).withSlot(0);
+
+    shooterConfig = new TalonFXConfiguration();
+    shooterConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+    shooterConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+    shooterConfig.CurrentLimits.SupplyCurrentLimit = 40.0;
+    shooterConfig.CurrentLimits.StatorCurrentLimit = 90.0;
+
+    shooterConfig.Slot0.kP = bruinRobotConfig.getTurretConfig().shooterKp;
+    shooterConfig.Slot0.kI = bruinRobotConfig.getTurretConfig().shooterKi;
+    shooterConfig.Slot0.kD = bruinRobotConfig.getTurretConfig().shooterKd;
+    shooterConfig.Slot0.kS = bruinRobotConfig.getTurretConfig().shooterKs;
+    shooterConfig.Slot0.kV = bruinRobotConfig.getTurretConfig().shooterKv;
+    shooterConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    shooterConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+
+    // change this later!
+    shooterConfig.MotionMagic.MotionMagicCruiseVelocity = 0.4550;
+    shooterConfig.MotionMagic.MotionMagicAcceleration = 0.41;
+
+    Phoenix6Util.applyAndCheckConfiguration(shooterMotor, shooterConfig, 5);
+    Phoenix6Util.applyAndCheckConfiguration(shooterMotor2, shooterConfig, 5);
+
+    shooterMotor2.setControl(follower);
+
+    shooterAppliedVoltage = shooterMotor.getMotorVoltage();
+    shooterSupplyCurrent = shooterMotor.getSupplyCurrent();
+    shooterStatorCurrent = shooterMotor.getStatorCurrent();
+    shooterVelocityRotationsPerSec = shooterMotor.getVelocity();
+    shooterAccelerationRotationsPerSecSquared = shooterMotor.getAcceleration();
+  
+    // type of motor, gear ratio, then moment of inertia
+    shooterSimFlywheel = new FlywheelSim(
+        DCMotor.getKrakenX60Foc(1),
+        4.0,
+        2.0);
+
+}
+
+  @Override
+  public void updateInputs(ShooterIOInputs inputs) {
+    BaseStatusSignal.refreshAll(
+        shooterAppliedVoltage,
+        shooterSupplyCurrent,
+        shooterStatorCurrent,
+        shooterVelocityRotationsPerSec,
+        shooterAccelerationRotationsPerSecSquared);
+
+    inputs.shooterVoltage = shooterAppliedVoltage.getValueAsDouble();
+    inputs.shooterSupplyCurrent = shooterSupplyCurrent.getValueAsDouble();
+    inputs.shooterStatorCurrent = shooterStatorCurrent.getValueAsDouble();
+    inputs.shooterVelocityRadPerSec =
+        Units.rotationsToRadians(shooterVelocityRotationsPerSec.getValueAsDouble());
+    inputs.shooterAccelRadPerSecSquared =
+        Units.rotationsToRadians(shooterAccelerationRotationsPerSecSquared.getValueAsDouble());
+  }
+
+  @Override
+  public void setVoltage(double volts) {
+    shooterMotor.setVoltage(volts);
+  }
+
+  @Override
+  public void setVelo(AngularVelocity velo) {
+    shooterMotor.setControl(motionMagicVelocityVoltage.withVelocity(velo));
+  }
+}
